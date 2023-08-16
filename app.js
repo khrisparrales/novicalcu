0759f1d1feb6d0c0f86281f0bfd7e501f1362399
Khris Parrales <36217164+khrisparrales@users.noreply.github.com>
2023-08-16 21:32:39 +0000

    agregado nuevos valor

--- app.js
+++ app.js
@@ -4,31 +4,42 @@ let valorsuma;
 let valorporcentaje;
 let valorbasesuma;
 let valor12;
-
+let valordebito;
 function myFunction() {
   /**convertir valor base a un decimal */
   /**validar que funcione con comas */
   valorBase = document.getElementById("base").value;
   valorBase = valorBase.replace(",", ".");
 
+
+
+
   valorBase = parseFloat(valorBase);
   console.log(valorBase);
   /**Al valor sumarle 40 */
-  valorsuma = valorBase + 40;
+  valorsuma = valorBase + 0;
 
   /**Al valor sumarle el 10% */
-  valorporcentaje = valorsuma + Math.floor(valorsuma * 10) / 100;
+  valorporcentaje = valorsuma + Math.floor(valorsuma * 6) / 100;
+  valordebito = valorsuma + Math.floor(valorsuma * 4) / 100;
+  let valor12c = valorsuma + Math.floor(valorsuma * 18) / 100;
+  let valor9c = valorsuma + Math.floor(valorsuma * 15) / 100;
+  let valor6c = valorsuma + Math.floor(valorsuma * 12) / 100;
+  let valor3c = valorsuma + Math.floor(valorsuma * 9) / 100;
   valorporcentaje = valorporcentaje.toFixed(3);
+  
   valorbasesuma = valorBase + Math.floor(valorBase * 10) / 100;
   valorbasesuma = valorbasesuma.toFixed(3);
   valor12 = valorBase - Math.floor(valorBase * 12) / 100;
   valor12 = valor12.toFixed(3);
 
   /**Mostrar el valor en la pagina */
-  document.getElementById("resultadosumatotal").innerHTML = valorsuma;
+  document.getElementById("resultadosumatotal").innerHTML = valordebito;
   document.getElementById("resultadosumaporcentaje").innerHTML =
     valorporcentaje;
-  document.getElementById("resultadobasesumaporcentaje").innerHTML =
-    valorbasesuma;
-  document.getElementById("resultadobasesumaporcentaje12").innerHTML = valor12;
+  document.getElementById("resultado12c").innerHTML =
+    valor12c;
+  document.getElementById("resultado9c").innerHTML = valor9c;
+  document.getElementById("resultado6c").innerHTML = valor6c;
+  document.getElementById("resultado3c").innerHTML = valor3c;
 }
